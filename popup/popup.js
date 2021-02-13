58b8e2a540700acc5563904c82709d840cf655d5
woog2roid <54667577+woog2roid@users.noreply.github.com>
2021-02-14 00:19:38 +0900

    popup UI update

---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+import {getData} from '../background/StorageControl'
+
+let list = document.getElementById('list');
+//const items = getData();
+
+list.innerText = "일단 아무거나";
+//list.innerText = items.toString;
