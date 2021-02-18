1cc2c7b9fe6f46b3d000e3c41c76f7710718507b
woog2roid <54667577+woog2roid@users.noreply.github.com>
2021-02-18 23:34:04 +0900

    v.1.1.0: details CRUD complete

--- detail/detail.js
+++ detail/detail.js
@@ -4,7 +4,11 @@ chrome.storage.sync.get({ tabBundleNameList: [] }, function (res) {
             const sectionContainer = document.getElementById('section');
             const detailsContainer = document.createElement("details");
             const summaryElement = document.createElement("summary");
+            
+            const TextCon = document.createElement("div");
             const newText = document.createTextNode(name);
+            TextCon.appendChild(newText);
+            
             sectionContainer.appendChild(detailsContainer);
             detailsContainer.appendChild(summaryElement);
             summaryElement.appendChild(newText);
@@ -12,6 +16,19 @@ chrome.storage.sync.get({ tabBundleNameList: [] }, function (res) {
             const formContainer = document.createElement("form");
             formContainer.id = name;
             detailsContainer.appendChild(formContainer);
+            
+            const addButton = document.createElement("img");
+            addButton.setAttribute("src", "../images/plus.svg");
+            addButton.className = "listAdder hoverable";
+            addButton.id = name + "button";
+            formContainer.appendChild(addButton);
+
+            const submit = document.createElement("input");
+            submit.setAttribute("type", "submit");
+            submit.setAttribute("value", "Submit Changes");
+            submit.className = "submit";
+            formContainer.appendChild(submit);
+
             option = {}; option[name] = [];
             chrome.storage.sync.get(option, function (urlsObject) {
                 urls = urlsObject[name];
@@ -25,11 +42,7 @@ chrome.storage.sync.get({ tabBundleNameList: [] }, function (res) {
                     liContainer.appendChild(tabsList);
                 })
 
-                const submit = document.createElement("input");
-                submit.setAttribute("type", "submit");
-                submit.setAttribute("value", "Change Items");
-                formContainer.appendChild(submit);
-            });                
+            });             
         });
     }
 });
@@ -38,13 +51,16 @@ window.onload = function () {
     const forms = document.getElementsByTagName("form");
     for (let i = 0; i < forms.length; i++) {
         forms[i].onsubmit = function () {
+            //alert("submit");
+            
             const item = forms[i];
-            //alert(item.id);
+            
             const inputs = document.getElementsByClassName(item.id);
             let urlArr = [];
             let isFormatted = true;
             for (let index = 0; index < inputs.length; index++){
                 //alert(inputs[index].value);
+                if (inputs[index].value.length == 0) continue;
                 if (inputs[index].value.indexOf("http://") == -1 && inputs[index].value.indexOf("https://") == -1) {
                     isFormatted = false;
                     inputs[index].value = "https://" + inputs[index].value; 
@@ -58,6 +74,23 @@ window.onload = function () {
 
             let option = {}; option[item.id] = urlArr;
             chrome.storage.sync.set(option);
+            
+        };
+    }
+
+    const listAdders = document.getElementsByClassName("listAdder");
+    for (let i = 0; i < listAdders.length; i++) {
+        listAdders[i].onclick = function () {
+            let id = listAdders[i].id.substring(0, (listAdders[i].id.length) - 6);
+            //alert(id);
+
+            formCon = document.getElementById(id);
+            const liContainer = document.createElement("li");
+            formCon.appendChild(liContainer);
+            let tabsList = document.createElement("input");
+            tabsList.className = id;
+            tabsList.setAttribute("size", 60);
+            liContainer.appendChild(tabsList);
         };
     }
 };
