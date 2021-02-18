9bcf15c2307615810e383683a14c7b1c42a60b6f
woog2roid <54667577+woog2roid@users.noreply.github.com>
2021-02-18 18:21:48 +0900

    Tab Bundle's url update function

--- detail/detail.js
+++ detail/detail.js
@@ -4,23 +4,51 @@ chrome.storage.sync.get({ tabBundleNameList: [] }, function (res) {
             const sectionContainer = document.getElementById('section');
             const detailsContainer = document.createElement("details");
             const summaryElement = document.createElement("summary");
-            summaryElement.id = name;
             const newText = document.createTextNode(name);
             sectionContainer.appendChild(detailsContainer);
             detailsContainer.appendChild(summaryElement);
             summaryElement.appendChild(newText);
             
+            const formContainer = document.createElement("form");
+            formContainer.id = name;
+            detailsContainer.appendChild(formContainer);
             option = {}; option[name] = [];
             chrome.storage.sync.get(option, function (urlsObject) {
                 urls = urlsObject[name];
                 urls.forEach(function (url) {
-                    let tabsList = document.createElement("li");
-                    let urlString = document.createTextNode(url);
-                    tabsList.appendChild(urlString);
-                    detailsContainer.appendChild(tabsList)
-                }) 
+                    const liContainer = document.createElement("li");
+                    formContainer.appendChild(liContainer);
+                    let tabsList = document.createElement("input");
+                    tabsList.className = name;
+                    tabsList.setAttribute("value", url);
+                    tabsList.setAttribute("size", 60);
+                    liContainer.appendChild(tabsList);
+                })
+
+                const submit = document.createElement("input");
+                submit.setAttribute("type", "submit");
+                submit.setAttribute("value", "Change Items");
+                formContainer.appendChild(submit);
             });                
         });
     }
 });
 
+window.onload = function () {
+    const forms = document.getElementsByTagName("form");
+    for (let i = 0; i < forms.length; i++) {
+        forms[i].onsubmit = function () {
+            const item = forms[i];
+            //alert(item.id);
+            const inputs = document.getElementsByClassName(item.id);
+            let urlArr = [];
+            for (let index = 0; index < inputs.length; index++){
+                urlArr.push(inputs[index].value);
+                alert(inputs[index].value);
+            }
+
+            let option = {}; option[item.id] = urlArr;
+            chrome.storage.sync.set(option);
+        };
+    }
+};
